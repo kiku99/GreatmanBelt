cd38ae7a24814079a7ec9546ef54eb11a617cebd
kiku99 <elsd0326@gmail.com>
2021-01-21 19:17:52 +0900

    데이터 입력, 인풋체크 기능 구현

--- app/src/main/java/com/example/a2020greatmanbelt/fragment/MainFragment.kt
+++ app/src/main/java/com/example/a2020greatmanbelt/fragment/MainFragment.kt
@@ -1,14 +1,20 @@
 package com.example.a2020greatmanbelt.fragment
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import com.example.a2020greatmanbelt.R
+import com.example.a2020greatmanbelt.data.NameEntity
+import com.example.a2020greatmanbelt.data.UserViewModel
 import kotlinx.android.synthetic.main.fragment_main_gragment.*
 import kotlinx.android.synthetic.main.fragment_main_gragment.view.*
 
@@ -16,7 +22,7 @@ import kotlinx.android.synthetic.main.fragment_main_gragment.view.*
 class MainGragment : Fragment() {
 
 
-    lateinit var navController: NavController
+    private lateinit var mUserViewModel: UserViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,12 +31,32 @@ class MainGragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_main_gragment, container, false)
 
-        view.StartBtn.setOnClickListener(){
-            findNavController().navigate(R.id.action_mainGragment_to_checklist)
-        }
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
 
+        view.StartBtn.setOnClickListener(){
+            insertDataToDatabase()
+        }
         return view
     }
 
+    private fun insertDataToDatabase(){
+        val name = edittext_name.text.toString()
+
+        if(inputCheck(name)){
+            //create user object
+            val user = NameEntity(0, name)
+            //add data to database
+            mUserViewModel.addUser(user)
+            Toast.makeText(requireContext(), "사용자 설정 완료", Toast.LENGTH_LONG).show()
+            // navigate back
+            findNavController().navigate(R.id.action_mainGragment_to_checklist)
+        }else{
+            Toast.makeText(requireContext(), "이름을 입력해 주세요!", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun inputCheck(name:String):Boolean{
+        return !(TextUtils.isEmpty(name))
+    }
 
 }
